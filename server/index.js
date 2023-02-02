4f09756926a986519815f054dff197f7013e08b8
Sean Wong <sean.uuong@gmail.com>
2023-02-01 21:57:24 -0800

    Complete post to repos endpoint

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ app.use(function(req, res, next) {
 // this server must serve those files when requested.
 
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
