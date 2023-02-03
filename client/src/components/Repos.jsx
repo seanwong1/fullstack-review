d042469dc8642eaea7007fe7460bdbcb35488212
Sean Wong <sean.uuong@gmail.com>
2023-02-02 20:50:04 -0800

    Complete repo name linking to github

--- client/src/components/Repos.jsx
+++ client/src/components/Repos.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Repos = ({ repo }) => {
   return (
     <div>
-      {repo.full_name}
+      <div>{repo.watchers_count}</div>
+      <a href={"https://github.com/" + repo.full_name}>{repo.full_name}</a>
     </div>
 )};
 
